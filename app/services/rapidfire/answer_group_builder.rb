21d0ed3afb1580eb0aa84d4f7145c0abbfe905ed
plombix <plombix@gmail.com>
2015-09-10 02:51:53 +0200

    add params leaker

--- app/services/rapidfire/answer_group_builder.rb
+++ app/services/rapidfire/answer_group_builder.rb
@@ -34,7 +34,7 @@ module Rapidfire
 
     def save(options = {})
       save!(options)
-      binding.pry
+      params[:mesresult] = @answer_group.id
     rescue ActiveRecord::ActiveRecordError => e
       # repopulate answers here in case of failure as they are not getting updated
       @answers = @question_group.questions.collect do |question|

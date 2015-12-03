fdbd7e93f603ca6d4e16731a8494d2eafed7b1ed
plombix <plombix@gmail.com>
2015-12-03 05:00:58 +0100

    debug

--- app/models/rapidfire/question.rb
+++ app/models/rapidfire/question.rb
@@ -2,6 +2,7 @@ module Rapidfire
   class Question < ActiveRecord::Base
     belongs_to :question_group, :inverse_of => :questions
     has_many   :answers
+    binding.pry
     
 
     default_scope { order(:position) }
